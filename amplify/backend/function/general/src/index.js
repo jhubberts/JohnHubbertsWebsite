c0b040eb9134f2708bbfb0d0febffe9c27462b45
John Hubberts <hubberts@amazon.com>
2020-04-09 19:06:18 -0700

    Added some ice cold CORS light

--- amplify/backend/function/general/src/index.js
+++ amplify/backend/function/general/src/index.js
@@ -5,6 +5,11 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        }
     };
+
     return response;
 };
