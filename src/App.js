d11d8e880703f14e34d0e243fc25e597ec7307b8
John Hubberts <hubberts@amazon.com>
2020-04-09 18:46:03 -0700

    Added a connectivity checker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ConnectivityChecker from './components/ConnectivityChecker';
 
 function App() {
   return (
     <div className="App">
       <h1>Under Renovation</h1>
+      <ConnectivityChecker/>
     </div>
   );
 }
