9ae648bee81dd97cf92a4b77d54dc4e8383ae6d0
John Hubberts <hubberts@amazon.com>
2020-04-14 16:51:05 -0700

    HTML5 Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Redirect, Route, BrowserRouter } from 'react-router-dom';
+import { Link, Switch, Redirect, Route, BrowserRouter } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav';
@@ -14,38 +14,33 @@ import ContactPage from "./components/ContactPage/ContactPage";
 import ProjectsPage from "./components/ProjectsPage/ProjectsPage";
 
 function App () {
-  const router = (
-    <BrowserRouter>
-      <Switch>
-        <Redirect exact path="/index.html" to="/"/>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/contact" component={ContactPage} />
-        <Route exact path="/projects" component={ProjectsPage} />
-        <Route exact path="/health" component={SiteHealth} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
-  );
-
   const navbar = (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/">John Hubberts</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">John Hubberts</Navbar.Brand>
       <Nav className="mr-auto">
-      <Nav.Link href="/projects">Projects</Nav.Link>
-      <Nav.Link href="/about">About</Nav.Link>
-      <Nav.Link href="/contact">Contact</Nav.Link>
+      <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+      <Nav.Link as={Link} to="/about">About</Nav.Link>
+      <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
     </Nav>
     </Navbar>
   );
 
   return (
-    <div>
+    <BrowserRouter>
       {navbar}
+
       <Container fluid>
-        {router}
+        <Switch>
+          <Redirect exact path="/index.html" to="/"/>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route exact path="/projects" component={ProjectsPage} />
+          <Route exact path="/health" component={SiteHealth} />
+          <Route component={NotFound} />
+        </Switch>
       </Container>
-    </div>
+    </BrowserRouter>
   );
 };
 
