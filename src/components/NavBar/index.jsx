678bf06998a86648fb731ed67f590e183592be77
John Hubberts <jhubberts@gmail.com>
2020-05-02 10:18:21 -0700

    Fixed import warning on react router dom Link

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar'
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
 
 import './NavBar.scss';
 
