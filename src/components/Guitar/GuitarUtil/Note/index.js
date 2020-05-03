cf212eed3c007a01a91c5e03b40cf60dce9d7a19
John Hubberts <jhubberts@gmail.com>
2020-05-03 11:06:40 -0700

    Compute note frequency

--- src/components/Guitar/GuitarUtil/Note/index.js
+++ src/components/Guitar/GuitarUtil/Note/index.js
@@ -19,10 +19,18 @@ const EQUIVALENCE = {
     "A#": "Bb"
 }
 
+const HALF_STEP = Math.pow(2, 1/12);
+const TUNING_A_FREQUENCY_HZ = 440;
+const DISTANCE_FROM_MIDDLE_C_TO_TUNING_A = 9;
+
+const NOTE_TO_FREQUENCY_LOOKUP = {};
+NOTE_TO_FREQUENCY_LOOKUP[DISTANCE_FROM_MIDDLE_C_TO_TUNING_A] = TUNING_A_FREQUENCY_HZ;
+
 class Note {
     constructor(props) {
         this.name = props.name;
         this.distanceFromMiddleC = props.distanceFromMiddleC;
+        this.frequency = Note.computeFrequency(this.distanceFromMiddleC);
     }
 
     static fromDistance(distanceFromMiddleC) {
@@ -34,6 +42,20 @@ class Note {
 
     transpose(transposeDistance) {
         return Note.fromDistance(this.distanceFromMiddleC + transposeDistance);
+    }
+
+    static computeFrequency(distanceFromMiddleC) {
+        // Lazy load
+        if (!NOTE_TO_FREQUENCY_LOOKUP[distanceFromMiddleC]) {
+            const distanceFromTuningA = distanceFromMiddleC - DISTANCE_FROM_MIDDLE_C_TO_TUNING_A;
+            if (distanceFromTuningA >= 0) {
+                NOTE_TO_FREQUENCY_LOOKUP[distanceFromMiddleC] = TUNING_A_FREQUENCY_HZ * Math.pow(HALF_STEP, distanceFromTuningA);
+            } else {
+                NOTE_TO_FREQUENCY_LOOKUP[distanceFromMiddleC] = TUNING_A_FREQUENCY_HZ / Math.pow(HALF_STEP, -distanceFromTuningA);
+            }
+        }
+
+        return NOTE_TO_FREQUENCY_LOOKUP[distanceFromMiddleC];
     }
 }
 
