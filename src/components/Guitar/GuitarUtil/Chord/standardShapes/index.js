ca43f637a5e8715597ae1c9a9f01763fa8bef69e
John Hubberts <jhubberts@gmail.com>
2020-05-04 17:56:39 -0700

    Added lots of shell chords

--- src/components/Guitar/GuitarUtil/Chord/standardShapes/index.js
+++ src/components/Guitar/GuitarUtil/Chord/standardShapes/index.js
@@ -1,3 +1,4 @@
+import shapes_6 from './6.json'
 import shapes_7b13 from './7b13.json'
 import shapes_7 from './7.json'
 import shapes_9 from './9.json'
@@ -5,11 +6,13 @@ import shapes_13 from './13.json'
 import shapes_dim7 from './dim7.json'
 import shapes_maj7 from './maj7.json'
 import shapes_maj9 from './maj9.json'
+import shapes_min6 from './min6.json'
 import shapes_min7 from './min7.json'
 import shapes_min7b5 from './min7b5.json'
 import shapes_min9 from './min9.json'
 
 const standardShapes = {
+    "6": shapes_6,
     "7": shapes_7,
     "7b13": shapes_7b13,
     "9": shapes_9,
@@ -17,6 +20,7 @@ const standardShapes = {
     "dim7": shapes_dim7,
     "maj7": shapes_maj7,
     "maj9": shapes_maj9,
+    "min6": shapes_min6,
     "min7": shapes_min7,
     "min7b5": shapes_min7b5,
     "min9": shapes_min9
